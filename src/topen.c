d01b742277572599b82d61dec0ef639091e1b1ec
debu1207 <patradebu1207@gmail.com>
2024-09-23 21:07:10 +0530

    Add comment and modes information

--- src/topen.c
+++ src/topen.c
@@ -5,13 +5,16 @@
 
 int main() {
 	int fd;
-	
+	// Try to open a fd of file tempfile. If doesn't exist, create the file.
 	fd = open("tempfile", O_RDWR | O_CREAT, 0755);
 
+	// Error handling. Always check the return value of syscalls.
 	if (fd < 0) {
 		printf("Error: %d\n", errno);
+		exit(1);
 	}
 
+	// Essential to always close the fd after using.
 	close(fd);
 
 	return 0;
