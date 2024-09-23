b8caa908f314c9c738d6d92c467572cd9a257edf
debu1207 <patradebu1207@gmail.com>
2024-09-23 21:22:07 +0530

    Add read() and info

---
+++ src/tread.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <errno.h>
+
+int main() {
+	int fd, count = 10;
+	ssize_t res;
+	char *buf;
+	buf = (char *)malloc(count + 1);
+
+	if (!buf) {
+		printf("Error on malloc(): %d\n", errno);
+		exit(1);
+	}
+		
+	fd = open("tempfile", O_RDWR | O_CREAT , 0755);
+	if (fd < 0) {
+		printf("Error on open(): %d\n", errno);
+		exit(1);
+	}
+
+	res = read(fd, buf, count);
+	buf[count] = '\0';
+
+	printf("Buffer read: %s\n", buf);
+
+	return 0;
+}
